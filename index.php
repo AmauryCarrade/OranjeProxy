AmauryCarrade/OranjeProxy

commit 69d461f930f4f645e0d94ed81c30cac4828f3b27

You see the unobfuscated link while hovering it (#8)

index.php
@@ -1101,6 +1101,20 @@ else {
 						$attrs['longdesc'] = complete_url($attrs['longdesc']);
 					}
 					break;
+				
+				case 'a':
+					if (isset($attrs['href'])) { // Juste in case... :p
+						$rebuild = true;
+						
+						if (!isset($attrs['title']) || empty($attrs['title'])) {
+							$attrs['title'] = $attrs['href'];
+						}
+						else {
+							$attrs['title'] .= ' (' . $attrs['href'] . ')';
+						}
+						
+						$attrs['href'] = complete_url($attrs['href']);
+					}
 
 				default:
 					foreach ($tags[$tag] as $attr) {

commit e20ddb050919c32e4c472856beac255e2793bdda

I just updated and OranjeProxy has been broken since 69d461f930f4f645e0d94ed81c30cac4828f3b27 cause of this little mistake - reference AmauryCarrade/OranjeProxy#8

index.php
@@ -1115,7 +1115,7 @@ else {
 						
 						$attrs['href'] = complete_url($attrs['href']);
 					}
-
+					break;
 				default:
 					foreach ($tags[$tag] as $attr) {
 						if (isset($attrs[$attr])) {