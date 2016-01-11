e7aa4f8f4d39febbdab89f80185099ac5784de4c
Machou <dokcharlie@gmail.com>
2016-01-11 16:51:56 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,32 +1,36 @@
 <?php
 /*
-	+-------------------+-------------------------------------------------------------------+
-	|  Script			| PHProxy   +   SabzProxy				|
-	|  Author			| Abdullah Arif						|
-	|  Modifier		| Forgetful  (Hamid R) + Timo Van Neerden + Amaury Carrade	|
-	|  Last Modified	| 11:55 PM 26/03/2014						|
-	+-------------------+-------------------------------------------------------------------+
-	|  This program is free software; you can redistribute it and/or			|
-	|  modify it under the terms of the GNU General Public License				|
-	|  as published by the Free Software Foundation; either version 2			|
-	|  of the License, or (at your option) any later version.				|
-	|											|
-	|  This program is distributed in the hope that it will be useful,			|
-	|  but WITHOUT ANY WARRANTY; without even the implied warranty of			|
-	|  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the			|
-	|  GNU General Public License for more details.						|
-	|											|
-	|  You should have received a copy of the GNU General Public License			|
-	|  along with this program; if not, write to the Free Software				|
-	|  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.		|
-	+---------------------------------------------------------------------------------------+
+	+-----------------------+-----------------------------------------------------------------------+
+	|  Script		| PHProxy   +   SabzProxy						|
+	|  Author		| Abdullah Arif								|
+	|  Modifier		| Forgetful  (Hamid R) + Timo Van Neerden + Amaury Carrade + Machou	|
+	|  Last Modified	| 11 january 2016							|
+	+-----------------------+-----------------------------------------------------------------------+
+	|  This program is free software; you can redistribute it and/or				|
+	|  modify it under the terms of the GNU General Public License					|
+	|  as published by the Free Software Foundation; either version 2				|
+	|  of the License, or (at your option) any later version.					|
+	|												|
+	|  This program is distributed in the hope that it will be useful,				|
+	|  but WITHOUT ANY WARRANTY; without even the implied warranty of				|
+	|  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the				|
+	|  GNU General Public License for more details.							|
+	|												|
+	|  You should have received a copy of the GNU General Public License				|
+	|  along with this program; if not, write to the Free Software					|
+	|  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.			|
+	+-----------------------------------------------------------------------------------------------+
 */
 
 // LANGUAGE
 //
 
-// add es for spain, ru for russian...etc
-$language_acceppted = Array('en', 'fr');
+// Please, if you want translate it, add same english & french, Russian > "ru", Spain > "es")
+// Find all identification codes here : http://www.metamodpro.com/browser-language-codes
+$language_acceppted = array(
+				'en',	// English
+				'fr'	// French
+			);
 
 function get_language($sDefault = 'en')
 {
